bf8f4e485f04d770ed600f3d04b4712b7df77c40
sjroh <sijine@gmail.com>
2015-01-22 11:39:29 -0600

    project1.cpp added

---
+++ project1.cpp
@@ -0,0 +1,15 @@
+// Si Jine Roh
+// Course and section
+// Due: January 30, 2015
+// Project1.cpp
+// compile code should be blahblah
+
+#include <iostream>
+
+use namespace std;
+
+int main(){
+	// FOR TDD START
+	cout("TDD START");
+	// FOR TDD END
+}
