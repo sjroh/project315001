61fdc7d0c69213a077774c9bd2acd0eea4cbba01
sjroh <sijine@gmail.com>
2015-01-22 13:56:45 -0600

    comments added

--- project1.cpp
+++ project1.cpp
@@ -3,13 +3,29 @@
 // Due: January 30, 2015
 // Project1.cpp
 // compile code should be blahblah
+// I've get the basic concept of this problem from the lecture and Wikipedia.
+// The way of solving this problem is find the shortest neighbor and keep moving on.
+// from the beginning, this program will find the shortest neighbor node to 
 
 #include <iostream>
-
-use namespace std;
+using namespace std;
 
 int main(){
+	// select file START
+	// TODO!!!
+	// select file END
+
+	// open file START
+		// if first char has #, then ignore the line
+		// else 
+	// open file END
+
+
+
+
 	// FOR TDD START
-	cout("TDD START");
+	cout << "TDD START";
 	// FOR TDD END
+
+	return 0;
 }
